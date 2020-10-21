52f649830552ab14966ae58539d125c3d8a2633e
Steffen Schultze <sschult@mpibpc.mpg.de>
2020-10-21 10:24:23 +0200

    add google sheets support

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, request, send_from_directory, render_template
 import os, csv, datetime, string
 
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 datapath = "data.csv"
 logpath = "coffee.log"
 template = {"coffee": 0, "tea": 0, "milk": 0}
+
+SPREADSHEET_ID = '1hAvLsZLNnDxm-8b3Dc5MyE6chK1kBngeLFIME7fcr9o'
+RANGE_NAME = 'Sheet1!A1:E100'
+
 
 app = Flask(__name__, static_url_path="")
 app.debug = True
 
 @app.route("/")
-def top():
+def route_top():
     return send_from_directory("static", "index.html")
 
 @app.route("/data.csv")
@@ -17,19 +27,24 @@ def route_data():
     return send_from_directory("", "data.csv")
 
 @app.route("/<path:path>")
-def func(path):
+def route_static(path):
     return send_from_directory("static", path)
 
 @app.route("/submit", methods = ["POST"])
 def route_form():
-    parsed = parseForm(request.form)
-    name = request.form["name"] if "name" in request.form else "unknown"
-    name = name.translate(str.maketrans('', '', string.punctuation))
-    row = updateData(data, name, parsed)
-    writeData(datapath, data)
-    with open(logpath, "a+") as fp:
-        fp.write(str(datetime.datetime.now()) + " " + name + ": " + str(parsed) + "\n")
-    return render_template("response.html", **row)
+    row = updateGoogleSheet(request.form)
+
+    # parsed = parseForm(request.form)
+    # name = request.form["name"] if "name" in request.form else "unknown"
+
+    # row = updateData(data, name, parsed)
+    # writeData(datapath, data)
+    # with open(logpath, "a+") as fp:
+    #     fp.write(str(datetime.datetime.now()) + " " + ": " + str(parsed) + "\n")
+    if row:
+        return render_template("response.html", message = "You now have:", message2 = f"Coffee: {row[1]}, Tea: {row[2]}, Milk: {50*row[3]+250*row[4]}ml")
+    else:
+        return render_template("response.html", message = "Name not found")
 
 
 def parseForm(form):
@@ -74,8 +89,67 @@ def writeData(path, data):
         for key, value in data.items():
             writer.writerow({"name": key, **value})
 
+def updateGoogleSheet(form):
+    name = request.form["name"]
+
+    values = getGoogleSheetValues()
+    names = [v[0] for v in values]
+
+    column_Names = [v.lower() for v in values[0]]
+
+    if name in names[1:]:
+        index = names[1:].index(name) + 1
+        row = values[index]
+
+        for key in form:
+            if key == "coffee1":
+                row[column_Names.index("coffee")] += 1
+            elif key == "coffee2":
+                row[column_Names.index("coffee")] += 2
+            elif key == "tea1":
+                row[column_Names.index("tea")] += 1
+            elif key == "milk50":
+                row[column_Names.index("milk 50 ml")] += 1
+            elif key == "milk250":
+                row[column_Names.index("milk 250 ml")] += 1
+        
+        sheet.values().update(spreadsheetId=SPREADSHEET_ID, range = f"Sheet1!B{index+1}:E{index+1}", body={'values': [row[1:]]}, valueInputOption = "RAW").execute()
+        return row
+    else:
+        return False
+
+
+def getGoogleSheetValues():
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, valueRenderOption="UNFORMATTED_VALUE").execute()
+    values = result.get('values', [])
+    for i in range(len(values)):
+        if len(values[i]) == 0:
+            values[i] = ["", 0, 0, 0, 0]
+    return values
+
+def getCreds():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
 
 data = {}
+
+creds = getCreds()
+
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
 
 if __name__ == '__main__':
     data = loadData(datapath)
